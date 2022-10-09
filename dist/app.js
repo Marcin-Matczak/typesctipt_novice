20af123301185200a8683ac3e9333f3ccc39c39c
Marcin <marcin@Scrap-Ubuntu-22.04-LTS>
2022-10-09 21:08:41 +0200

    ToDo App: Changed tasks to objects and add checkboxes

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,53 @@
-/*<li>
-<label for="task-1">Learn TypeScript</label>
-<input type="checkbox" id="task-1" name="Learn TypeScript" />
-</li>*/
 const taskNameInputElement = document.querySelector("#name");
 const addButtonElement = document.querySelector(".add");
 const taskContainerElement = document.querySelector(".tasks");
-const tasks = ["Learn TypeScript", "Book plane flight", "Learn for the exam"];
+// ustawienie typowania w tablicy zawierajacej obiekty
+const tasks = [
+    {
+        name: "Learn TypeScript",
+        done: false,
+    },
+    {
+        name: "Book plane flight",
+        done: true,
+    },
+    {
+        name: "Learn for the exam",
+        done: false,
+    },
+];
 const render = () => {
     taskContainerElement.innerHTML = "";
-    tasks.forEach((task) => {
+    tasks.forEach((task, index) => {
+        /*<li>
+            <label for="task-1">Learn TypeScript</label>
+            <input type="checkbox" id="task-1" name="Learn TypeScript" />
+        </li>*/
         const taskElement = document.createElement("li");
-        taskElement.innerText = task;
+        const id = `task-${index}`;
+        const labelElement = document.createElement("label");
+        labelElement.innerHTML = task.name;
+        // ustawiamy nazwe atrybutu for na wartosc stalej id
+        labelElement.setAttribute("for", id);
+        const checkboxElement = document.createElement("input");
+        checkboxElement.type = "checkbox";
+        checkboxElement.name = "task.name";
+        checkboxElement.id = id;
+        checkboxElement.checked = task.done;
+        checkboxElement.addEventListener("change", () => {
+            task.done = !task.done;
+        });
+        taskElement.appendChild(labelElement);
+        taskElement.appendChild(checkboxElement);
         taskContainerElement.appendChild(taskElement);
     });
 };
-const addTask = (task) => {
-    tasks.push(task);
+const addTask = (taskName) => {
+    tasks.push({ name: taskName, done: false });
 };
 addButtonElement.addEventListener('click', (event) => {
     event.preventDefault();
-    tasks.push(taskNameInputElement.value);
+    addTask(taskNameInputElement.value);
     render();
 });
 render();
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,67 @@
-/*<li>
-<label for="task-1">Learn TypeScript</label>
-<input type="checkbox" id="task-1" name="Learn TypeScript" />
-</li>*/
-
 const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
 const addButtonElement: HTMLButtonElement = document.querySelector(".add");
 const taskContainerElement: HTMLElement = document.querySelector(".tasks");
 
-const tasks: string[] = ["Learn TypeScript","Book plane flight","Learn for the exam"];
+// ustawienie typowania w tablicy zawierajacej obiekty
+const tasks: {
+    name: string;
+    done: boolean;
+}[] = [
+    {
+        name: "Learn TypeScript",
+        done: false,
+    },
+    {
+        name: "Book plane flight",
+        done: true,
+    },
+    {
+        name: "Learn for the exam",
+        done: false,
+    },
+];
 
 const render = () => {
     taskContainerElement.innerHTML = "";
-    tasks.forEach((task) => {
-        const taskElement: HTMLElement = document.createElement("li");
-        taskElement.innerText = task;
+    tasks.forEach((task, index) => {
+
+        /*<li>
+            <label for="task-1">Learn TypeScript</label>
+            <input type="checkbox" id="task-1" name="Learn TypeScript" />
+        </li>*/
+
+        const taskElement: HTMLElement = document.createElement("li");  
+        const id: string = `task-${index}`;
+
+        const labelElement: HTMLLabelElement = document.createElement("label");        
+        labelElement.innerHTML = task.name;
+
+        // ustawiamy nazwe atrybutu for na wartosc stalej id
+        labelElement.setAttribute("for", id);
+
+        const checkboxElement: HTMLInputElement = document.createElement("input");
+        checkboxElement.type = "checkbox";
+        checkboxElement.name = "task.name";
+        checkboxElement.id = id;
+        checkboxElement.checked = task.done;
+        checkboxElement.addEventListener("change", () => {
+            task.done = !task.done;
+        })
+
+        taskElement.appendChild(labelElement);
+        taskElement.appendChild(checkboxElement);
+
         taskContainerElement.appendChild(taskElement);
     });
 };
 
-const addTask = (task: string) => {
-    tasks.push(task);
+const addTask = (taskName: string) => {
+    tasks.push({name: taskName, done: false});
 }
 
 addButtonElement.addEventListener('click', (event: Event) => {
     event.preventDefault();
-    tasks.push(taskNameInputElement.value);
+    addTask(taskNameInputElement.value);
     render();
 })
 
